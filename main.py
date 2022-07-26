93e9241fade8edba5ecb0f0039a1b66a87c711a1
Kapusta-fairy <dknenorm63@gmail.com>
2022-07-26 17:44:32 +0300

    add tests

--- main.py
+++ main.py
@@ -21,61 +21,70 @@ class Conversation(VanessasCore):
 
     def __send_text(self, chat_id, text):
         self.api_session.messages.send(chat_id=chat_id, message=text, random_id=0)
+        return text
 
     def __send_stick(self, chat_id, stick_id):
         self.api_session.messages.send(chat_id=chat_id, sticker_id=stick_id, random_id=0)
+        return stick_id
 
     def __send_file(self, chat_id, url):
         self.api_session.messages.send(chat_id=chat_id, attachment=url, random_id=0)
+        return url
 
     def _remove_msg(self, peer_id, msg_id):
         self.api_session.messages.delete(peer_id=peer_id, conversation_message_ids=msg_id, delete_for_all=True, random_id=0)
+        return msg_id
 
     def __send_random_fraction(self, chat_id):
-        self.__send_text(chat_id, f'🎲 {herofractions[randint(0, 7)]}')
+        rnd_fraction = f'🎲 {herofractions[randint(0, 7)]}'
+        self.__send_text(chat_id, rnd_fraction)
+        return rnd_fraction
 
     def __send_roll_dice(self, chat_id, msg):
         if msg.replace(dice, '').isdigit() and msg.replace(dice, '') != '0':
-            self.__send_text(chat_id, f'🎲 {randint(1, int(msg.replace(dice, "")))}')
+            roll_dice = f'🎲 {randint(1, int(msg.replace(dice, "")))}'
+            self.__send_text(chat_id, roll_dice)
+            return roll_dice
 
     def __send_wiki_article(self, chat_id, msg):
         try:
-            self.__send_text(chat_id, summary(msg.replace('что такое', ''), sentences=2))
+            article = summary(msg.replace('что такое', ''), sentences=4)
+            self.__send_text(chat_id, article)
+            return article
         except PageError:
             self.__send_text(chat_id, PageError_response)
+            return PageError
         except DisambiguationError:
             self.__send_text(chat_id, DisambiguationError_response)
+            return DisambiguationError
 
     def __shut_up(self, chat_id, msg, peer_id, event):
         members = self.__members_search(peer_id, chat_id)
         if self.__from_check(event, members):
-            if not members:
-                return
             victim_id = self.__victim_id_search(msg, event)
             if victim_id == 'ub2121387' or victim_id == '-212138773':
-                self.__send_text(chat_id, 'я бы сказала, что это несколько возмутительно')
-                return
+                return self.__send_text(chat_id, 'я бы сказала, что это несколько возмутительно')
             found = False
             for member in members['items']:
                 try:
                     if str(member['member_id']) == victim_id:
                         found = True
                         if member['is_admin']:
-                            self.__send_file(chat_id, img_no_power)
-                        return
+                            return self.__send_file(chat_id, img_no_power)
+                        break
                 except KeyError:
                     pass
             if not found:
-                self.__send_text(chat_id, 'жертвы нету в этой беседе')
+                return self.__send_text(chat_id, 'жертвы нету в этой беседе')
             elif victim_id in self._shut_up_people:
                 self.__send_text(chat_id, f'наш [id{victim_id}|друг] уже отдыхает')
-                return
+                return f'наш [id{victim_id}|друг] уже отдыхает'
             else:
                 self._shut_up_people.append(victim_id)
                 self.__send_text(chat_id, f'наш [id{victim_id}|друг] пока что отдохнет')
-                return
+                return f'наш [id{victim_id}|друг] пока что отдохнет'
         else:
-            self.__send_file(chat_id, img_no_power)
+            return self.__send_file(chat_id, img_no_power)
 
     def __redemption(self, chat_id, msg, event, peer_id):
         members = self.__members_search(peer_id, chat_id)
@@ -84,8 +93,10 @@ class Conversation(VanessasCore):
             if victim_id in self._shut_up_people:
                 self._shut_up_people.remove(victim_id)
                 self.__send_text(chat_id, f'[id{victim_id}|друг], ты свободен, наслаждайся жизнью и хорошего тебе дня')
+                return f'[id{victim_id}|друг], ты свободен, наслаждайся жизнью и хорошего тебе дня'
             else:
-                self.__send_text(chat_id, f'да не то чтобы он сильно замучен')
+                self.__send_text(chat_id, 'да не то чтобы он сильно замучен')
+                return 'да не то чтобы он сильно замучен'
         else:
             self.__send_file(chat_id, img_no_power)
 
@@ -105,10 +116,10 @@ class Conversation(VanessasCore):
     def __members_search(self, peer_id, chat_id):
         try:
             members = self.api_session.messages.getConversationMembers(peer_id=peer_id)
+            return members
         except ApiError:
             self.__send_text(chat_id, 'ну знаете, могли бы админку чтоле дать для начала')
             return
-        return members
 
     def __victim_id_search(self, msg, event):
         if 'раз' in msg:
@@ -126,47 +137,45 @@ class Conversation(VanessasCore):
 
     def response_definition(self, chat_id, msg, peer_id, event):
         if msg in text_commands:
-            self.__send_text(chat_id, text_commands[msg])
+            return self.__send_text(chat_id, text_commands[msg])
 
         elif msg in gifs_commands:
-            self.__send_file(chat_id, gifs_commands[msg])
+            return self.__send_file(chat_id, gifs_commands[msg])
 
         elif msg in img_commands:
-            self.__send_file(chat_id, img_commands[msg])
+            return self.__send_file(chat_id, img_commands[msg])
 
         elif msg in stick_commands:
-            self.__send_stick(chat_id, stick_commands[msg])
+            return self.__send_stick(chat_id, stick_commands[msg])
 
         elif msg == heroes_helper[0]:
-            self.__send_random_fraction(chat_id)
+            return self.__send_random_fraction(chat_id)
 
         elif msg == heroes_helper[1]:
-            self.__send_text(chat_id, position[randint(0, 3)])
+            return self.__send_text(chat_id, position[randint(0, 3)])
 
         elif msg[:1] == dice:
-            self.__send_roll_dice(chat_id, msg)
+            return self.__send_roll_dice(chat_id, msg)
 
         elif msg == aboba:
-            self.__send_text(chat_id, zmiysphrases[randint(0, 14)])
+            return self.__send_text(chat_id, zmiysphrases[randint(0, 14)])
 
         elif msg[:9] in wiki:
-            self.__send_wiki_article(chat_id, msg)
+            return self.__send_wiki_article(chat_id, msg)
 
         elif msg[:3] == mute[0]:
-            self.__shut_up(chat_id, msg, peer_id, event)
+            return self.__shut_up(chat_id, msg, peer_id, event)
 
         elif msg[:6] == mute[1]:
-            self.__redemption(chat_id, msg, event, peer_id)
+            return self.__redemption(chat_id, msg, event, peer_id)
 
         for i in indirect_gifs_command:
             if i in msg:
-                self.__send_file(chat_id, indirect_gifs_command[i])
-                return
+                return self.__send_file(chat_id, indirect_gifs_command[i])
 
         for i in indirect_img_commands:
             if i in msg:
-                self.__send_file(chat_id, indirect_img_commands[i])
-                return
+                return self.__send_file(chat_id, indirect_img_commands[i])
 
 
 class Launcher(Conversation):
---
+++ tests/test_mute.py
@@ -0,0 +1,58 @@
+import main
+from pytest import mark
+from mock import patch
+
+Vanessa = main.Launcher()
+test_chat_id = 4
+test_cases = [
+    [(test_chat_id, 'мут', 'наш [id560930170|друг] пока что отдохнет')],
+    [(test_chat_id, 'мут', 'я бы сказала, что это несколько возмутительно')],
+    [(test_chat_id, 'мут', 'photo-212138773_457239033')],
+]
+members = {"items": [{"member_id": 465630601, "is_admin": True},
+                     {"member_id": 560930170, "is_admin": False},
+                     {"member_id": -212138773, "is_admin": True}]}
+
+
+@mark.parametrize('chat_id, command, expect_response', test_cases[0])
+def test_mute(chat_id, command, expect_response):
+    with patch('main.Conversation._Conversation__members_search') as members_mock:
+        members_mock.return_value = members
+        with patch('main.Conversation._Conversation__victim_id_search') as victim_id_mock:
+            victim_id_mock.return_value = '560930170'
+            with patch('main.Conversation._Conversation__from_check') as from_check_mock:
+                from_check_mock.return_value = True
+                assert Vanessa.response_definition(chat_id, command, 0, 0) == expect_response
+
+
+@mark.parametrize('chat_id, command, expect_response', test_cases[1])
+def test_mute_vanessa(chat_id, command, expect_response):
+    with patch('main.Conversation._Conversation__members_search') as members_mock:
+        members_mock.return_value = members
+        with patch('main.Conversation._Conversation__victim_id_search') as victim_id_mock:
+            victim_id_mock.return_value = '-212138773'
+            with patch('main.Conversation._Conversation__from_check') as from_check_mock:
+                from_check_mock.return_value = True
+                assert Vanessa.response_definition(chat_id, command, 0, 0) == expect_response
+
+
+@mark.parametrize('chat_id, command, expect_response', test_cases[2])
+def test_mute_admin(chat_id, command, expect_response):
+    with patch('main.Conversation._Conversation__members_search') as members_mock:
+        members_mock.return_value = members
+        with patch('main.Conversation._Conversation__victim_id_search') as victim_id_mock:
+            victim_id_mock.return_value = '465630601'
+            with patch('main.Conversation._Conversation__from_check') as from_check_mock:
+                from_check_mock.return_value = True
+                assert Vanessa.response_definition(chat_id, command, 0, 0) == expect_response
+
+
+@mark.parametrize('chat_id, command, expect_response', test_cases[2])
+def test_mute_from_not_admin(chat_id, command, expect_response):
+    with patch('main.Conversation._Conversation__members_search') as members_mock:
+        members_mock.return_value = members
+        with patch('main.Conversation._Conversation__victim_id_search') as victim_id_mock:
+            victim_id_mock.return_value = '560930170'
+            with patch('main.Conversation._Conversation__from_check') as from_check_mock:
+                from_check_mock.return_value = False
+                assert Vanessa.response_definition(chat_id, command, 0, 0) == expect_response
