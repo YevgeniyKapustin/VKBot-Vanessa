96a42bfee196b693948e85278738e724f4a3fe55
Kapusta-fairy <dknenorm63@gmail.com>
2023-03-24 13:25:06 +0300

    refactor add_command.py

--- vanessa/commands_logic/add_command.py
+++ vanessa/commands_logic/add_command.py
@@ -1,28 +1,27 @@
+"""Module for interactive addition of commands by users."""
 from sqlite3 import IntegrityError
 
 from basic_actions.actions import send_text
 from basic_actions.database import DataBase
+from service_files.big_strings import commands_list_postfix
 
 
 class Commands(object):
-    """Class for working with custom commands"""
+    """Class for working with custom commands.
+
+    :Methods:
+    - get_commands()
+    - add_command(msg, event)
+    - remove_command(msg, chat_id)
+    """
 
     def __init__(self):
         self.db = DataBase()
 
-    def get_commands(self):
-        """Returns all commands from db"""
+    def get_commands(self) -> str:
+        """Return all commands from db."""
         prefix = 'Команды на текущий момент:<br>Запрос, Ответ, Тип, Контекст'
-        postfix = f'''Прочие команды:<br>
-        фракция - генерирует случайную фракцию из героев 5
-        д* - генерирует случайное число в диапазоне от 1 до указанного числа
-        что такое* - отвечает первыми тремя предложениями из википедии
-        мут* - удаляет все новые сообщения этого пользователя(только для админов)
-        размут* - выключает мут для этого пользователя(только для админов)
-        добавить команду помощь - показывает информацию о добавлении команд
-        рарити - отправляет случайную рарити
-        абоба - абоба
-        команды - показывает полный список команд'''
+        postfix = commands_list_postfix
         commands = ''
 
         for index, i in enumerate(self.db.get_all_commands_data()):
@@ -30,51 +29,64 @@ class Commands(object):
 
         return f'{prefix}<br><br>{commands}<br><br>{postfix}'
 
-    def add_command(self, msg: str, chat_id, event):
-        """Example: добавить команду текст_внутри вопрос: ответ"""
+    def add_command(self, msg: str, event) -> str or None:
+        """Add command to db.
+
+        :param msg: example: "добавить команду текст_ запрос: ответ"
+        :param event: object with information about the event
+        """
+        chat_id = event.chat_id
         try:
-            request, response, _type = self.define_command(msg)
-            strategy = 'contextual'
+            request, response, _type = self._define_command(msg)
 
-            if request == 'сус':
-                return send_text(chat_id, 'сус священен')
-            elif 'добавить команду' in request:
-                return send_text(chat_id, 'неа')
-            elif 'удалить команду' in request:
-                return send_text(chat_id, 'неа')
+            if self._define_prohibitions(request, chat_id):
+                return None
 
-            if _type[-1] == '_':
+            strategy = self._define_strategy(_type)
+            if strategy == 'contextual':
                 _type = _type[:-1]
-            else:
-                strategy = 'normal'
 
-            if _type == 'гиф':
-                attachment = event.message.attachments[0]['doc']
-                response = f'doc{attachment["owner_id"]}_{attachment["id"]}'
-            elif _type == 'изображение':
-                attachment = event.message.attachments[0]['photo']
-                response = f'photo{attachment["owner_id"]}_{attachment["id"]}'
+            attch = self._define_attachment(_type, event)
+            if attch:
+                response = f'{_type}{attch["owner_id"]}_{attch["id"]}'
 
-            if request and response and _type:
+            if request and response and _type and strategy:
                 try:
                     self.db.set_command(_type, strategy, request, response)
                 except IntegrityError:
                     self.db.update_command(_type, strategy, request, response)
                 return send_text(chat_id, f'команда {request} была добавлена')
             else:
-                return self.__some_wrong(chat_id)
-        except IndexError or ValueError:
-            return self.__some_wrong(chat_id)
+                return self._something_wrong(chat_id)
+        except (IndexError, ValueError):
+            return self._something_wrong(chat_id)
 
-    @staticmethod
-    def __some_wrong(chat_id):
-        return send_text(chat_id, 'чета ты насусил братик')
+    def remove_command(self, msg: str, chat_id: id) -> str:
+        """Remove command from db.
 
-    def define_command(self, msg):
-        msg: list = self._filtering(msg.lower().split(' '))
+        :param msg: example: удалить команду текст запрос
+        :param chat_id: id of the chat to which the message will be sent
+        """
+        msg = msg.split(':')
+        request = msg[0].strip()
 
-        _type = self._get_command_type(msg)
-        msg.remove(msg[0])
+        if request == 'сус':
+            return send_text(chat_id, 'сус священен')
+        if request:
+            self.db.remove_command(request)
+            return send_text(chat_id, f'команда {request} была удалена')
+        else:
+            return send_text(chat_id, 'чета ты насусил братик')
+
+    def _define_command(self, msg: str) -> tuple:
+        """Extract data from a message.
+
+        :param msg: message sent by user
+        """
+        data: list = self._filtering(msg)
+
+        _type: str = self._get_command_type(data)
+        data.remove(msg[0])
 
         msg: list = ' '.join(msg).split(':')
 
@@ -84,48 +96,30 @@ class Commands(object):
         return request, response, _type
 
     @staticmethod
-    def _get_command_type(msg):
-        try:
-            return msg[0]
-        except IndexError as error:
-            return error
+    def _something_wrong(chat_id: int) -> str:
+        """Send an error message.
 
-    def remove_command(self, msg, chat_id):
-        """Example: удалить команду текст запрос"""
-
-        msg = msg.split(':')
-        request = msg[0].strip()
-
-        if request == 'сус':
-            return send_text(chat_id, 'сус священен')
-
-        if request:
-            self.db.remove_command(request)
-            return send_text(chat_id, f'команда {request} была удалена')
-        else:
-            return send_text(chat_id, 'чета ты насусил братик')
+        :param chat_id: id of the chat to which the message will be sent
+        """
+        return send_text(chat_id, 'чета ты насусил братик')
 
     @staticmethod
-    def _command_type_definition(msg, chat_id):
-        if msg == 'добавить команду помощь':
-            add_help = '''добавить команду (тип ответа)(_) (запрос): (ответ)
-                       например: добавить команду текст_ амогус: тутуту
-                       нижнее подчеркивание нужно добавить если вы хотите, 
-                       чтобы команда писалась в случае если запрос содержится 
-                       в сообщении, а не целиком его составляла в случае ответа
-                       для гиф или изображения нужен его url, для стикера id 
-                       типы команд: текст, гиф, изображение, стикер'''
-            send_text(chat_id, add_help)
-            return None
-        try:
-            _type = msg[0]
-        except IndexError as error:
-            return error
+    def _get_command_type(data: list) -> str:
+        """Return type from command data.
 
-        return _type
+        :param data: command data
+        """
+        try:
+            return data[0]
+        except IndexError as error:
+            return str(error)
 
     @staticmethod
-    def _filtering(msg) -> list:
+    def _filtering(msg: str) -> list:
+        """Remove redundant information from the message.
+
+        :param msg: message sent by user
+        """
         if not isinstance(msg, list):
             msg = msg.lower().split(' ')
         msg.remove('команду')
@@ -134,3 +128,48 @@ class Commands(object):
         else:
             msg.remove('удалить')
         return msg
+
+    @staticmethod
+    def _define_strategy(_type: str) -> str:
+        """Determine the command strategy.
+
+        :param _type: raw command type
+        """
+        if _type[-1] == '_':
+            return 'contextual'
+        else:
+            return 'normal'
+
+    @staticmethod
+    def _define_prohibitions(request: str, chat_id: int) -> str or None:
+        """Determine the command strategy.
+
+        :param request: request for a new command
+        :param chat_id: id of the chat to which the message will be sent
+        :return: string if found prohibitions else None
+        """
+        if request == 'сус':
+            return send_text(chat_id, 'сус священен')
+        elif 'добавить команду' in request:
+            return send_text(chat_id, 'неа')
+        elif 'удалить команду' in request:
+            return send_text(chat_id, 'неа')
+        return None
+
+    @staticmethod
+    def _define_attachment(_type: str, event):
+        """Determine the command strategy.
+
+        :param _type: command type
+        :param event: object with information about the event
+        """
+        if _type == 'гиф':
+            attch_type = 'doc'
+        elif _type == 'изображение':
+            attch_type = 'photo'
+        else:
+            return None
+
+        attachment = event.message.attachments[0][f'{attch_type}']
+
+        return attachment
