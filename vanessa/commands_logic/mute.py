dc8de051f1c77839184ab9e5c74d373258305949
Kapusta-fairy <dknenorm63@gmail.com>
2023-03-25 13:39:02 +0300

    refactor mute.py

--- vanessa/commands_logic/mute.py
+++ vanessa/commands_logic/mute.py
@@ -6,93 +6,90 @@ from prepare.connection import Connection
 
 
 class Mute(object):
-    """Class for working with mutes and unmutes"""
+    """Class for working with mutes and unmutes.
 
-    def __init__(self, msg, event):
+    :Methods:
+    shut_up()
+    redemption()
+    """
+    def __init__(self, event):
         self.chat_id = event.chat_id
-        self.text = msg.text
-        self.peer_id = msg.peer_id
-        self.img_no_power = ''
+        self.text = event.msg.text
+        self.peer_id = event.msg.peer_id
         self.vk = Connection().vk
         self.db = DataBase()
         self.event = event
-        self.msg = msg
+        self.msg = event.msg
 
-    def shut_up(self):
-        """Allows conversation admins to mute a non-admin conversation
-        participant
+    def shut_up(self) -> str:
+        """Added to mutes a non-admin conversation participant.
+
+        Sends a command report.
         """
-        members = self.__members_search()
+        members = self._members_search()
 
-        if self.__member_status_check(members):
-            victim_id = self.__victim_id_search()
+        if self._member_status_check(members):
+            victim_id = self._victim_id_search()
             # vanessa can't shut up
             if victim_id == 'ub2121387' or victim_id == '-212138773':
-                return send_text(self.chat_id, 'я бы сказала, что это '
-                                               'несколько возмутительно')
+                return self._send_response('mute_for_bot')
 
             found = False
 
             for member in members['items']:
                 try:
-                    if str(member['member_id']) == victim_id:
+                    if member['member_id'] == int(victim_id):
                         found = True
                         if member['is_admin']:
-                            return send_file(self.chat_id, self.img_no_power)
+                            return self._send_response('no_power')
                         break
                 except KeyError:
                     pass
 
             if not found:
-                return send_text(self.chat_id, 'жертвы нету в этой беседе')
+                return self._send_response('no_found_victim', victim_id)
 
             elif victim_id == self.db.get_shut_up_person(victim_id):
-                send_text(self.chat_id,
-                          f'наш [id{victim_id}|друг] уже отдыхает')
-                return f'наш [id{victim_id}|друг] уже отдыхает'
+                return self._send_response('already_muted', victim_id)
 
             else:
                 self.db.set_shut_up_person(victim_id)
-                send_text(self.chat_id,
-                          f'наш [id{victim_id}|друг] пока что отдохнет')
-                return f'наш [id{victim_id}|друг] пока что отдохнет'
+                return self._send_response('muted', victim_id)
 
         else:
-            return send_file(self.chat_id, self.img_no_power)
+            return self._send_response('no_power')
 
-    def redemption(self):
-        """Allows an admin to remove a member's mute"""
-        members = self.__members_search()
+    def redemption(self) -> str:
+        """Allow an admin to remove a member's mute.
 
-        if self.__member_status_check(members):
-            victim_id = self.__victim_id_search()
+        Sends a command report.
+        """
+        members = self._members_search()
+
+        if self._member_status_check(members):
+            victim_id = self._victim_id_search()
             if int(victim_id) == self.db.get_shut_up_person(victim_id)[0]:
                 self.db.remove_from_shut_up_people(victim_id)
-                return send_text(
-                    self.chat_id,
-                    f'[id{victim_id}|друг], ты свободен, наслаждайся жизнью и '
-                    'хорошего тебе дня'
-                )
+                return self._send_response('redemption')
             else:
-                return send_text(self.chat_id,
-                                 'да не то чтобы он сильно замучен')
+                return self._send_response('already_unmuted')
         else:
-            send_file(self.chat_id, self.img_no_power)
+            return self._send_response('no_power')
 
-    def __victim_id_search(self):
+    def _victim_id_search(self) -> str:
         text = self.text
         if 'раз' in text:
             text = text.replace('раз', '')
         text = text.replace('мут', '')
         if text == '' and 'reply_message' in self.event.object.message:
-            victim_id = str(self.msg.reply_message['from_id'])
+            victim_id = self.msg.reply_message['from_id']
         else:
-            victim_id = self.__id_definition_by_mention(text)
+            victim_id = self._id_definition_by_mention(text)
 
-        return victim_id
+        return str(victim_id)
 
-    def __member_status_check(self, members) -> bool:
-        """Returns true if the requester is an administrator"""
+    def _member_status_check(self, members) -> bool:
+        """Return true if the requester is an administrator."""
         from_admin = False
         member_id = self.msg.from_id
 
@@ -106,17 +103,42 @@ class Mute(object):
 
         return from_admin
 
-    def __members_search(self):
-        """Return object members or send about error"""
+    def _members_search(self):
+        """Return object members or send about error."""
         try:
             return self.vk.messages.getConversationMembers(
-                peer_id=self.peer_id)
-
+                peer_id=self.peer_id
+            )
         except ApiError:
-            return send_text(self.chat_id, 'ну знаете, могли бы админку чтоле '
-                                           'дать для начала')
+            return self._send_response('no_admin')
+
+    def _send_response(self, option: str, victim_id: str = '0') -> str:
+        """Report status of commands.
+
+        For the already_muted, muted, redemption, must be passed victim_id.
+        Sends text except in the case of no_power.
+        Options: mute_for_bot, no_found_victim, already_unmuted, already_muted,
+        muted, redemption, no_power, no_admin.
+
+        :param option: a string specifying the response choice
+        :param victim_id: vk id the victim of mute
+        """
+        options = {
+            'mute_for_bot': 'я бы сказала, что это несколько возмутительно',
+            'no_found_victim': 'жертвы нету в этой беседе',
+            'already_unmuted': 'да не то чтобы он сильно замучен',
+            'already_muted': f'наш [id{victim_id}|друг] уже отдыхает',
+            'muted': f'наш [id{victim_id}|друг] пока что отдохнет',
+            'redemption': f'[id{victim_id}|друг], ты свободен, наслаждайся '
+                          'жизнью и хорошего тебе дня',
+            'no_power': 'photo-212138773_457239033',
+            'no_admin': 'ну знаете, могли бы админку чтоле дать для начала',
+        }
+        if option == 'no_power':
+            return send_file(self.chat_id, options[option])
+        return send_text(self.chat_id, options[option])
 
     @staticmethod
-    def __id_definition_by_mention(mention):
-        """Cut id from mention"""
+    def _id_definition_by_mention(mention):
+        """Cut id from mention."""
         return mention.strip()[3:12]
