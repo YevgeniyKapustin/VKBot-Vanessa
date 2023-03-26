f8a56d623a56a83a7b6cb94c579438614728f92d
Kapusta-fairy <dknenorm63@gmail.com>
2023-03-26 13:30:30 +0300

    refactor wiki.py

--- vanessa/commands_logic/wiki.py
+++ vanessa/commands_logic/wiki.py
@@ -6,27 +6,63 @@ from wikipedia import summary, set_lang, PageError, DisambiguationError, \
 
 from basic_actions.actions import send_text
 
-set_lang('ru')
 
+class Wikipedia(object):
+    """Class for handling requests to work with wikipedia.
 
-def send_wiki_article(chat_id: int, msg: str) -> str:
-    """Send the article requested from wikipedia.
+        :param event: object with information about the event
 
-    :param chat_id: id of the chat to which the message will be sent
-    :param msg: message sent by user
+    Methods:
+    send_wiki_article()
     """
-    if 'что такое' in msg:
-        msg = msg.replace('что такое', '')
-    elif 'кто такой' in msg:
-        msg = msg.replace('кто такой', '')
-    elif 'кто такая' in msg:
-        msg = msg.replace('кто такая', '')
-    try:
-        response = summary(msg, chars=400)[::-1][:3:-1]
-        return send_text(chat_id, f'{response[:response.rfind(".")]}.')
-    except PageError:
-        return send_text(chat_id, 'чота нету ничего')
-    except DisambiguationError as error:
-        send_wiki_article(chat_id, choice(error.options))
-    except WikipediaException:
-        return send_text(chat_id, 'сусня какая-то')
+    set_lang('ru')
+
+    def __init__(self, event):
+        self.text = self._filtring(event.msg.text)
+        self.chat_id = event.chat_id
+
+    @staticmethod
+    def _wiki_exception_handler(func):
+        """Calls exception handling in Wikipedia class.
+
+        Highly specialized decorator for one specific function.
+        :param func: send_wiki_article method from Wikipedia class
+        """
+
+        def wrapper(self):
+            try:
+                func(self)
+            except PageError:
+                return self._handling_page_error()
+            except DisambiguationError as error:
+                return self._handling_disambiguation(error)
+            except WikipediaException:
+                return self._handling_wiki_exception()
+
+        return wrapper
+
+    @_wiki_exception_handler
+    def send_wiki_article(self) -> str:
+        """Send the article requested from wikipedia."""
+        response = summary(self.text, chars=400)[::-1][:3:-1]
+        return send_text(self.chat_id, f'{response[:response.rfind(".")]}.')
+
+    def _handling_disambiguation(self, error):
+        self.text = choice(error.options)
+        return self.send_wiki_article()
+
+    def _handling_page_error(self):
+        return send_text(self.chat_id, 'чота нету ничего')
+
+    def _handling_wiki_exception(self):
+        return send_text(self.chat_id, 'сусня какая-то')
+
+    @staticmethod
+    def _filtring(text):
+        if 'что такое' in text:
+            text = text.replace('что такое', '')
+        elif 'кто такой' in text:
+            text = text.replace('кто такой', '')
+        elif 'кто такая' in text:
+            text = text.replace('кто такая', '')
+        return text
