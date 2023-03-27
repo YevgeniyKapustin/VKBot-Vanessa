39c49593556627dac1d8f6793b80d3c2270aea6d
Kapusta-fairy <dknenorm63@gmail.com>
2023-03-27 13:37:36 +0300

    add test actions

--- vanessa/basic_actions/actions.py
+++ vanessa/basic_actions/actions.py
@@ -12,8 +12,11 @@ def send_text(chat_id: int, text: str) -> str:
     :param chat_id: id of the chat to which the message will be sent
     :param text: message sent by user
     """
-    _vk.messages.send(chat_id=chat_id, message=text, random_id=0)
-    return text
+    try:
+        _vk.messages.send(chat_id=chat_id, message=text, random_id=0)
+        return text
+    except ApiError as error:
+        return send_text(chat_id, f'{error}')
 
 
 def send_stick(chat_id: int, stick_id: int) -> int:
@@ -26,8 +29,8 @@ def send_stick(chat_id: int, stick_id: int) -> int:
         _vk.messages.send(chat_id=chat_id, sticker_id=int(stick_id),
                           random_id=0)
         return stick_id
-    except ApiError:
-        send_text(chat_id, f'чота не дает мне вк кинуть {stick_id} стик')
+    except (ApiError, ValueError) as error:
+        send_text(chat_id, f'{error}')
 
 
 def send_file(chat_id: int, url: str) -> str:
@@ -40,17 +43,17 @@ def send_file(chat_id: int, url: str) -> str:
     try:
         _vk.messages.send(chat_id=chat_id, attachment=url, random_id=0)
         return url
-    except ApiError:
-        return send_text(chat_id, 'чота сусня какая-то, вк ругаетя что '
-                                  'сообщение пустое либо у меня нет к нему '
-                                  'доступа, хотя я точно знаю, что сообщение '
-                                  f'содержит {url}')
+    except ApiError as error:
+        return send_text(chat_id, f'{error}')
 
 
 def remove_msg(event) -> int:
     """Deletes the message from the chat and returns the message id."""
-    return _vk.messages.delete(
-        peer_id=event.msg.peer_id,
-        conversation_message_ids=event.msg.conversation_message_id,
-        delete_for_all=True, random_id=0
-    )
+    try:
+        return _vk.messages.delete(
+            peer_id=event.msg.peer_id,
+            conversation_message_ids=event.msg.conversation_message_id,
+            delete_for_all=True, random_id=0
+        )
+    except ApiError as error:
+        return send_text(event.chat_id, f'{error}')
