2cc437a4991bb6002b76a40f3aa306c318014a8a
Kapusta-fairy <dknenorm63@gmail.com>
2023-03-30 10:21:11 +0300

    fix test name

--- tests/test_events.py
+++ tests/test_events.py
@@ -2,7 +2,7 @@ from basic_actions.events import EventBuilder
 from vars_for_test import MockEvent
 
 
-def test_remove_from_shut_up_people():
+def test_building_event():
     event = MockEvent
     assert (
         EventBuilder().set_msg(event.msg).set_chat_id(event.chat_id).
