b138b9e63ca54456ffea750389aabc6e2467bc90
Yevgeniy Kap <kapustaevg@gmail.com>
2023-10-02 09:06:24 +0300

    refactor event

--- src/utils/events.py
+++ src/utils/events.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from vk_api.bot_longpoll import VkBotMessageEvent
+from vk_api.vk_api import VkApiMethod
 
 from src.utils import vk
 
@@ -18,14 +19,16 @@ class Event(object):
     message: Message
     chat_id: int
     attachments: dict
+    api: VkApiMethod
 
     def __init__(self, message, chat_id, attachments=None):
         self.message = message
         self.chat_id = chat_id
         self.attachments = attachments
+        self.api = vk.get_bot_api()
 
     def answer(self, text):
-        vk.get_bot_api().messages.send(chat_id=self.chat_id, message=text, random_id=0)
+        self.api.messages.send(chat_id=self.chat_id, message=text, random_id=0)
 
 
 def extract_msg_from_event(event: VkBotMessageEvent) -> Message:
