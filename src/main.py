77a2ee112739b407709c13047677667ea9ca40b0
Yevgeniy Kap <kapustaevg@gmail.com>
2023-11-04 11:38:27 +0300

    add doc for main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
+"""Модуль для основного класса бота."""
 from loguru import logger
-from requests import ReadTimeout
-from requests.exceptions import ProxyError, ConnectionError
-from vk_api import ApiError
 from vk_api.bot_longpoll import VkBotEventType
 
 from src.services import vk
@@ -10,12 +8,18 @@ from src.services.events import Event, extract_msg_from_event, Message
 
 
 class Bot(object):
+    """Главный класс Ванессы, ведет логи.
 
+    Методы:
+    launch -- запускает бота
+
+    """
     def __init__(self):
         logger.add('.log')
 
     @logger.catch()
     def launch(self):
+        """Запускает бота: прослушивает эвенты, отлавливает исключения."""
         while True:
             try:
                 logger.info('launch...')
