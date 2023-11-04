52196fa376d6441fff647626bfeafc9ec03200da
Yevgeniy Kap <kapustaevg@gmail.com>
2023-11-04 11:34:16 +0300

    add doc for controller

--- src/utils/controller.py
+++ src/utils/controller.py
@@ -1,14 +1,24 @@
+"""Модуль для класса Controller."""
 from src.services.events import Event
 from src import handlers
 
 
 class Controller(object):
+    """Класс для работы с хэнделарми.
+
+    Поля:
+    event -- объект класс Event, с которым будем работать
+
+    Методы:
+    recognition -- инициирует проверку всех хэндлеров по текущему event
+    """
     __event: Event
 
     def __init__(self, event):
         self.__event = event
 
     def recognition(self):
+        """Инициирует проверку всех хэндлеров."""
         handlers_modules: list = self.__get_handlers_modules()
         self.__call_handlers(handlers_modules)
 
