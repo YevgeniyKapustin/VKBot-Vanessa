ca206c43e9711571d771a98a06abe8020c31c8ad
Yevgeniy Kap <kapustaevg@gmail.com>
2023-11-04 11:44:22 +0300

    add doc for config

--- src/config.py
+++ src/config.py
@@ -1,12 +1,15 @@
+"""Модуль для класса Settings."""
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    COMMUNITY_TOKEN: str
-    ADMIN_TOKEN: str
-    SERVER_URI: str
-    GROUP_ID: int
+    """Конфигурация бота."""
+    COMMUNITY_TOKEN: str  # Токен от сообщества, который будет использовать бот
+    ADMIN_TOKEN: str      # Токен юзера, который может пригодится
+    SERVER_URI: str       # Адрес сервера, с которым бот будет работать
+    GROUP_ID: int         # ID сообщества в ВК
 
     class Config:
+        """Настройка файла .env."""
         env_file = '../.env'
         env_file_encoding = 'utf-8'
